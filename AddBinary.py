cf9c7096c7272b7de3f29256b8e47f5bc9dabcf6
Dylan Coxshall <63456774+DCoxshall@users.noreply.github.com>
2023-03-22 23:43:57 +0000

    Create AddBinary.py

---
+++ AddBinary.py
@@ -0,0 +1,5 @@
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        return "{0:b}".format(int(a, 2) + int(b, 2))
+# I wrote this in three minutes while drunk
+# God exists and He is me.
