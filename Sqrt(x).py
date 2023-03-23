f02464c4b23dfcf93b295ffb4fcda2db23df5d59
Dylan Coxshall <63456774+DCoxshall@users.noreply.github.com>
2023-03-23 00:53:49 +0000

    Create Sqrt(x).py

---
+++ Sqrt(x).py
@@ -0,0 +1,6 @@
+import math
+
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        return math.floor(math.sqrt(x))
+# NGL this one was also pretty easy but I was drunk so even typing these words is an achievemnt as far as I'm concerned
